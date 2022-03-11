ae12649fd5d98f235e1cc65ce39b034c3cf82e7d
Владислав Андреевич Минаков <s0160144@edu.kubsu.ru>
2022-03-11 21:40:10 +0300

    куе

--- laba5_DZ/Program.fs
+++ laba5_DZ/Program.fs
@@ -2,12 +2,18 @@
 
 open System
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+let myans =function
+| "F#"|"Prolog"->"Чел, ну ты и подлиза"
+|"Pascal"->"Вы случайно не Кирилл Цветков?"
+|"Python"->"На дудке игрец?"
+|"Java"->"Пройдемте на конференцию"
+|"Swift"->"Apple ушла из рынка("
+|"Php"->"Хорош,  ты купил слона"
+|_->"Еще что расскажешь?"
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
+    printfn "Какой ваш любимый язык?"
+    let ans=Console.ReadLine()
+    Console.WriteLine(myans ans)
     0 // return an integer exit code
