a67a29455860c0c638063356c88c0f5c6056d5a8
Владислав Андреевич Минаков <s0160144@edu.kubsu.ru>
2022-03-11 22:46:45 +0300

    kjjk

---
+++ 12/Program.fs
@@ -0,0 +1,25 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    printfn "Какой твой любимый язык программирования?"
+    let answer x  = 
+        match x with 
+        | "F#"|"Prolog"->"Чел, ну ты и подлиза"
+        |"Pascal"->"Вы случайно не Кирилл Цветков?"
+        |"Python"->"На дудке игрец?"
+        |"Java"->"Пройдемте на конференцию"
+        |"Swift"->"Apple ушла из рынка("
+        |"Php"->"Хорош,  ты купил слона"
+        |_->"Еще что расскажешь?"    
+    //12.1
+    (Console.ReadLine>>answer>>Console.WriteLine)()
+    //12.2
+    //let z =Console.ReadLine()
+    let pot (x:string->unit) y z =  x(y (z()))
+    pot Console.WriteLine answer Console.ReadLine
+
+    
+    0 // return an integer exit code
