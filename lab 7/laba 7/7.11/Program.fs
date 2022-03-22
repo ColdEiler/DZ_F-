bbde29c2d1a47162bc1424f7e20827abfafae8e3
Владислав Андреевич Минаков <s0160144@edu.kubsu.ru>
2022-03-22 17:58:02 +0300

    еххуу 7 лаба

---
+++ lab 7/laba 7/7.11/Program.fs
@@ -0,0 +1,46 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+//1.9
+//Дан целочисленный массив. Необходимо найти элементы, расположенные перед последним минимальным.
+let rec readlist n =
+    if n =0 then []
+    else
+        let Head= int (Console.ReadLine ())
+        let Tail = readlist (n-1)
+        Head::Tail
+
+
+let readData= 
+    printfn "Сколько элементов в списке ?"
+    let n = int (Console.ReadLine())
+    readlist n
+
+let rec writeList = function
+|[] ->   let z = System.Console.ReadKey()
+         0
+| (head : int)::tail -> 
+                   Console.WriteLine(head)
+                   writeList tail
+
+let makenewlist list  last=
+    let rec r1 list last newlist ind= 
+        match list with 
+        |[]->newlist
+        |h::tail->
+            if(ind <> last) 
+            then 
+                let newnewlist= newlist @ [h]
+                r1 tail last newnewlist (ind+1) 
+            else
+                r1 [] last newlist ind
+    r1 list last [] 0
+
+[<EntryPoint>]
+let main argv =
+    let l = readData
+    let last= List.findIndexBack (fun x-> if (x= List.max l) then true else false) l
+    let newlist = makenewlist l last
+    Console.WriteLine()
+    writeList newlist 
+    0 // return an integer exit code
---
+++ lab 7/laba 7/7.12/Program.fs
@@ -0,0 +1,32 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+//1.19
+//Дан целочисленный массив.
+//Необходимоосуществить циклический сдвиг элементов массива вправо на одну позицию.
+
+let rec readlist n =
+    if n =0 then []
+    else
+        let Head= Console.ReadLine ()
+        let Tail = readlist (n-1)
+        Head::Tail
+
+
+let readData= 
+    printf "Сколько элементов в списке ?"
+    let n= int(Console.ReadLine ())
+    readlist n
+
+let rec writeList = function
+|[] ->   let z = System.Console.ReadKey()
+         0
+| (head : int)::tail -> 
+                   System.Console.WriteLine(head)
+                   writeList tail
+
+
+[<EntryPoint>]
+let main argv =
+  
+    0 // return an integer exit code
