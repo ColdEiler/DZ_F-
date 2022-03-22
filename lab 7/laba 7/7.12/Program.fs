fe1dfecba699cfa90415de56ecd821d216be9e90
Владислав Андреевич Минаков <s0160144@edu.kubsu.ru>
2022-03-22 18:14:11 +0300

    сделано

--- lab 7/laba 7/7.12/Program.fs
+++ lab 7/laba 7/7.12/Program.fs
@@ -8,13 +8,13 @@ open System
 let rec readlist n =
     if n =0 then []
     else
-        let Head= Console.ReadLine ()
+        let Head= int (Console.ReadLine ())
         let Tail = readlist (n-1)
         Head::Tail
 
 
 let readData= 
-    printf "Сколько элементов в списке ?"
+    printfn "Сколько элементов в списке ?"
     let n= int(Console.ReadLine ())
     readlist n
 
@@ -25,8 +25,20 @@ let rec writeList = function
                    System.Console.WriteLine(head)
                    writeList tail
 
+let dob list n = 
+    let rec dob1 list newlist ind=
+        match list with
+        |[]->newlist
+        |h::tail->
+            if(ind<> n)
+            then dob1 tail (newlist@[h]) (ind+1)
+            else dob1 [] newlist ind
+    dob1 list [] 0
 
 [<EntryPoint>]
 let main argv =
-  
+    let l= readData
+    let newlist = dob l (List.length l - 1)
+    writeList ([l.Item(List.length l - 1)] @ newlist)
+     
     0 // return an integer exit code
