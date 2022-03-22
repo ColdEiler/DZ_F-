5d34a9e87c750b4557c75dcaed8078c82c27f17d
Владислав Андреевич Минаков <s0160144@edu.kubsu.ru>
2022-03-22 21:57:04 +0300

    вспомнил о существовании фильтра)

---
+++ lab 7/laba 7/7.14/Program.fs
@@ -0,0 +1,45 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+//1.39
+//Дан целочисленный массив. Необходимо вывести вначале его
+//элементы с четными индексами, а затем - с нечетными.
+let rec readlist n =
+    if n =0 then []
+    else
+        let Head= int (Console.ReadLine ())
+        let Tail = readlist (n-1)
+        Head::Tail
+
+
+let readData= 
+    printfn "Сколько элементов в списке ?"
+    let n= int(Console.ReadLine ())
+    readlist n
+
+let rec writeList = function
+|[] ->   let z = System.Console.ReadKey()
+         0
+| (head : int)::tail -> 
+                   System.Console.WriteLine(head)
+                   writeList tail
+
+let normList list=
+    let rec r list newlist = 
+        match list with 
+        |[]->newlist
+        |(h:int*int)::tail->
+            let newnewlist = newlist @ [snd h]
+            r tail newnewlist
+    r list []
+
+[<EntryPoint>]
+let main argv =
+    let l = readData
+    let l2= List.indexed l 
+    let chlist = List.filter(fun x -> if fst x % 2 =0 then true else false) l2
+    let nechlist = List.filter(fun x-> if fst x % 2 <> 0 then true else false) l2
+    let l3 = chlist@nechlist
+    let l4 = normList l3
+    writeList l4
+    0 // return an integer exit code
