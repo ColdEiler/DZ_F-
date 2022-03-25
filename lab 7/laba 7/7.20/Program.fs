2bea7a20be97e2ada213724f9716b2ce334a3e50
Владислав Андреевич Минаков <s0160144@edu.kubsu.ru>
2022-03-25 22:22:18 +0300

    задание 9

--- lab 7/laba 7/7.20/Program.fs
+++ lab 7/laba 7/7.20/Program.fs
@@ -25,7 +25,7 @@ let readData =
     printfn "Сколько элементов в списке?"
     let n = int(Console.ReadLine())
     readList n 
-
+//10
 let tr s = 
     let rec r (s:string) n acc =
         match n with 
@@ -42,11 +42,45 @@ let tri list =
         |(h:string)::tail->
             r tail (newlist @ [tr h])
     r list []
+//10
+//11
+let kod (s:char) = int (s)
+
+let maxkod s =
+    let rec r (s:string) n acc =
+        match n with 
+        |(-1)->acc
+        |_->
+            if (kod(s.[n])>acc) then r s (n-1) (kod(s.[n]))
+            else r s (n-1) acc
+    r s ((String.length s)-1) 0       
+
+let maxdiff s =
+    let rec r (s:string) n acc =
+        match n with 
+        |(-1)->acc
+        |_->
+            let diff = kod(s.[n])-kod(s.[(String.length s)- 1- n])
+            if (kod(s.[n])>acc) then r s (n-1)  diff
+            else r s (n-1) acc
+    r s ((String.length s)-1) 0  
+
+let lala list = 
+    let rec r list newlist = 
+        match list with 
+        |[]-> newlist
+        | (h:string)::tail ->
+            r tail (newlist @ [(maxkod h - maxdiff h)*(maxkod h - maxdiff h)])
+    r list []
+
+
 
 [<EntryPoint>]
 let main argv =
     let l = readData
     let chislo, l2 = List.unzip (List.sort(List.zip (tri l) l))
-    writeList l2 
-    
+    writeList l2
+    Console.WriteLine()
+    let cchislo, l3 = List.unzip (List.sort (List.zip (lala l) l))
+    writeList l3 
     0 // return an integer exit code
